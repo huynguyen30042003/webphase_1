48fff358ad3f3e582c8762f0a4e5f95cdea8929d
huynguyen30042003 <nguyenkhoanhthuy@gmail.com>
2023-03-11 00:38:40 +0700

    11/3

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App() {
           <div className='nav__box'>
             <div className='nav'>
               <ul>
+                <img src={List}></img>
                 <li><a>Çözüm ve Hizmetler</a></li>
                 <li><a>Ürünler</a></li>
                 <li><a>Teknolojiler</a></li>
